43eae3903856c5eaf3563dfc7b2e0f45580a6890
Hiteshree-Chauhan-it <hiteshreechauhanit@gmail.com>
2026-02-25 10:41:42 +0530

    Create input_and_output.cpp

---
+++ 01_Introduction/input_and_output.cpp
@@ -0,0 +1,16 @@
+/*
+Problem Name: Input and Output
+Platform: HackerRank
+Domain: C++ (Introduction)
+Concept Used: Basic I/O (cin, cout)
+Difficulty: Easy
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin>>a>>b>>c; 
+    cout<<a+b+c; //Output = Sum of three numbers
+    return 0;
+}
